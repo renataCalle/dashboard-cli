252cca5f572d700c64c8faf813fbdefd2bd2f93b
valory <renatacallef@gmail.com>
2022-11-14 18:18:31 -0500

    managed device size tracking to handle sidebar

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,3 +1,4 @@
+import { initial } from 'lodash';
 import React, { createContext, useContext, useState} from 'react';
 
 const StateContext = createContext();
@@ -11,12 +12,19 @@ const initialState = {
 
 export const ContextProvider = (({children}) => {
     const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState);
+    const handleClick = (clicked) => {setIsClicked({...initialState, [clicked]: true})};
+    const [screenSize, setScreenSize] = useState(undefined);
 
     return (
         <StateContext.Provider 
         value= {{
             activeMenu,
-            setActiveMenu
+            setActiveMenu,
+            isClicked, 
+            setIsClicked,
+            handleClick,
+            screenSize, setScreenSize
         }}>
             {children}
         </StateContext.Provider>
