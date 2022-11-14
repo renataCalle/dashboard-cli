c554c4fb534526084aec68be5fca21f26efb522a
valory <renatacallef@gmail.com>
2022-11-13 23:19:41 -0500

    set up ContextProvider

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ import {Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Line,
 Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import './App.css'
 
+import { useStateContext } from './contexts/ContextProvider';
+
 const App = () => {
-  const activeMenu = true;
+  const {activeMenu} = useStateContext();
   return (
     <div>
         <BrowserRouter>
