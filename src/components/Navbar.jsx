2df2b68598242f009cb9919da6c9f38e0c84f978
valory <renatacallef@gmail.com>
2023-01-25 23:42:53 -0500

    changed names

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-import avatar from '../data/avatar.jpg';
+import avatar3 from '../data/avatar3.png';
 import { Cart, Chat, Notification, UserProfile} from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 
@@ -77,11 +77,11 @@ const Navbar = () => {
         onClick={()=>handleClick ('userProfile')}>
           <img
           className='rounded-full w-8 h-8'
-          src={avatar} 
+          src={avatar3} 
           />
           <p>
             <span className='text-gray-400 text-14'>Hi, </span> {' '}
-            <span className='text-gray-400 font-bold ml-1 text-14'>Michael</span>
+            <span className='text-gray-400 font-bold ml-1 text-14'>John</span>
           </p>
         </div>
       </TooltipComponent>
